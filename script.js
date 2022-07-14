2aadf490000eb85e6caa8963042f63cecfb2df96
CodingChase <pratyushagarwal4@gmail.com>
2022-07-14 18:17:15 +0530

    Implemented bubblesort and some ui changes

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let bars_container = document.getElementById("bars_container")
 
 // height range
 
-let minRange = 0 ;
-let maxRange = 200 ;
+let minRange = 1 ;
+let maxRange = 20;
+let height_factor = 20 ;
 
 // Bar Count and Creating Array of same length
 
-let numOfBars = 10 ;
+let numOfBars = 100;
 let unsorted_array = new Array(numOfBars);
 
 // Returns a random number between minRange and maxRange with both corners included
@@ -35,7 +36,7 @@ function renderBars(array){
 
         let bar = document.createElement("div");
         bar.classList.add("bar");
-        bar.style.height = `${array[i]}px` ;
+        bar.style.height = array[i]*height_factor + "px" ;
 
         bars_container.appendChild(bar);
 
@@ -57,3 +58,48 @@ randomize_array_btn.addEventListener("click",function(){
     renderBars(unsorted_array);
 }) ;   
 
+function sleep(ms) {
+
+    return new Promise (
+        (resolve) => setTimeout(resolve,ms));
+}
+
+async function bubbleSort(array) {
+
+    let bars = document.getElementsByClassName("bar");
+
+    for ( let i = 0 ; i < array.length ; i++) {
+
+        for (let j=0 ; j < array.length - i - 1 ; j++){
+
+            for(let k=0 ; k < array.length ;k++){
+
+                if(k!=j || k!=j+1)
+                    bars[k].style.backgroundColor = "aqua" ;
+            }
+
+            if(array[j] > array[j+1]){
+
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp ;
+
+                bars[j].style.height = array[j]*height_factor + "px";
+                bars[j].style.backgroundColor = "lightgreen";
+
+                bars[j + 1].style.height = array[j + 1]*height_factor + "px";
+                bars[j + 1].style.backgroundColor = "lightgreen";
+
+                await sleep(30);
+            }
+        }
+    }
+
+    return array ;
+
+}
+
+sort_btn.addEventListener("click" ,function(){
+
+    bubbleSort(unsorted_array);
+})
